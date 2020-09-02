b313871ca92764fa933e133c99de44accb1053bd
MKnyazev <MKnyazev@MKNYAZEV>
2020-09-02 23:34:48 +0300

    Unit test progect carryed in main solution.

---
+++ HookahNet.Tests/CurrencyTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using HookahNet.Model.Entities;
+
+namespace HookahNet.Tests
+{
+    public class CurrencyTests
+    {
+        [Fact]
+        public void DefaultCurrencyIsNotNull()
+        {
+            //Arrange
+            var Currency = new Currency();
+            //Assert
+            Assert.NotNull(Currency.CurrencyName);
+        }
+
+        [Fact]
+        public void SetAndValidateCurrency()
+        {
+            //Arrange
+            var Currency = new Currency();
+            //Act
+            Currency.SetCurrency(Currency.Currencies.EUR);
+            //Assert
+            Assert.Equal("EUR", Currency.CurrencyName);
+            Assert.Equal(1, Currency.CurrencyValue);
+        }
+    }
+}
