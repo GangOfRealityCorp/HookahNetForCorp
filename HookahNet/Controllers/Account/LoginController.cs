8b44c498223484d063a2e4401eb363016db5f3a2
GangOfReality <70590498+GangOfReality@users.noreply.github.com>
2020-10-01 22:04:02 +0300

    Created autorization via token.

--- HookahNet/Controllers/Account/LoginController.cs
+++ HookahNet/Controllers/Account/LoginController.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.EntityFrameworkCore;
 
 using HookahNet.Controllers.DBContexts;
 using HookahNet.Models;
 using HookahNet.Controllers.ControllerModels;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HookahNet.Controllers.Account
 {
     [Route("Account/{controller}")]
     [ApiController]
-    public class LoginController : ControllerBase
+    public class LoginController : Controller
     {
         private readonly StoreContext context;
         public LoginController(StoreContext context)
@@ -30,15 +33,42 @@ namespace HookahNet.Controllers.Account
         [HttpPost]
         public async Task<IActionResult> LoginGuestUser([FromBody] LoginModel loginModel)
         {
-            var guestUser = await context.guestUserTable.FirstOrDefaultAsync((user) => user.Email == loginModel.Email);
-            if(guestUser == null)
-                return Conflict($"User with Email '{loginModel.Email}' not found");
+            bool isIdentity = await GetIdentity(loginModel);
 
-            if (guestUser.Password != loginModel.Password)
-                return Conflict($"Password incorrect");
+            if(!isIdentity)
+                return BadRequest($"Invalid username or password.");
 
-            return Ok("Login successful");
-            //TODO: responce token.
+            var response = new
+            {
+                access_token = GenerateToken(loginModel),
+                username = loginModel.Email
+            };
+
+            return Json(response);
+        }
+
+        private async Task<bool> GetIdentity(LoginModel loginModel)
+        {
+            var guestUser = await context.guestUserTable.FirstOrDefaultAsync((user) => user.Email == loginModel.Email && user.Password == loginModel.Password);
+            if (guestUser == null)
+                return false;
+            return true;
+        }
+
+        private string GenerateToken(LoginModel loginModel)
+        {
+            var currentTime = DateTime.UtcNow;
+            // create JWT-token
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: currentTime,
+                    claims: null,
+                    expires: currentTime.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return encodedJwt;
         }
     }
 }
---
+++ HookahNet/Controllers/Account/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HookahNet.Controllers.Account
+{
+    [Route("Account/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult ValidateStatus()
+        {
+            return Ok("everything is ok");
+        }
+    }
+}
